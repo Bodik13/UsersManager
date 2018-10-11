df862f756ce78971d6ad2ab66cbcb312fcf71b7b
Bohdan Hutsul <bhuts@softserveinc.com>
2018-10-11 13:23:07 +0300

    Initial Commit

---
+++ UsersManager/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UsersManager
+//
+//  Created by Bohdan Hutsul on 10/11/18.
+//  Copyright © 2018 Bohdan Hutsul. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
