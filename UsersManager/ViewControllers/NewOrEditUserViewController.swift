425a02840e98f6943a911bb8a71b561103ecef91
Bohdan Hutsul <bhuts@softserveinc.com>
2018-10-12 17:11:58 +0300

    rename ViewController

--- UsersManager/ViewControllers/NewOrEditUserViewController.swift
+++ UsersManager/ViewControllers/NewOrEditUserViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class NewEditUserViewController: UIViewController {
+class NewOrEditUserViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
